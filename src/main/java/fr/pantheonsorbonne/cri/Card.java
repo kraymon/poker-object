bb569c62bcc535ca2831d74ed4c91567fe35dc79
Abdel Malik Djendli <a.malik.djendli@gmail.com>
2022-09-26 21:43:11 +0200

    Modification class Card

--- src/main/java/fr/pantheonsorbonne/cri/Card.java
+++ src/main/java/fr/pantheonsorbonne/cri/Card.java
@@ -1,5 +1,31 @@
 package fr.pantheonsorbonne.cri;
 
 public class Card {
-    
+
+    public String couleur;
+    public String valeur;
+
+    public Card(String valeur,String couleur){
+        this.valeur = valeur;
+        this.couleur = couleur;
+    }
+
+    public String getValeur(){
+        return this.valeur;
+    }
+
+    public String getCouleur(){
+        return this.couleur;
+    }
+
+    public void setValeur(String valeur){
+     this.valeur = valeur;
+    }
+
+    public void setCouleur(String couleur){
+     this.couleur = couleur;
+    }
+
+
+
 }
