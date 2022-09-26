ed2c2ffb36ccdcd6ebd3fcbcab1c4f5283d3e47b
kraymon <raymond.ken11@gmail.com>
2022-09-26 22:42:04 +0200

    player commit

--- src/main/java/fr/pantheonsorbonne/cri/Player.java
+++ src/main/java/fr/pantheonsorbonne/cri/Player.java
@@ -1,5 +1,34 @@
 package fr.pantheonsorbonne.cri;
 
 public class Player {
+    private String name;
+
+    public Player(String name){
+        this.name=name;
+    } 
+
+    public String getName(){
+        return this.name;
+    }
+
+    private Card[] hand = new Card[5];
+    public void setHand(Card[] main){
+        //à definire
+        this.hand=main;
+    }
+
+    public void getCardsToDiscard(){
+        //à definire
+        //for(Card)
+    }
+
+    public void addCard(){
+        //à definire
+    }
+
+    public void getHandString(){
+        //à definire
+    }
+
     
 }
