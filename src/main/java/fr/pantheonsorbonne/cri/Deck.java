9d8e0ceabdc8e54d3e1219bdc436e943449ddb8c
Abdel Malik Djendli <a.malik.djendli@gmail.com>
2022-09-26 22:38:14 +0200

    DECK modif

--- src/main/java/fr/pantheonsorbonne/cri/Deck.java
+++ src/main/java/fr/pantheonsorbonne/cri/Deck.java
@@ -1,5 +1,26 @@
 package fr.pantheonsorbonne.cri;
-
+import java.util.Random;
 public class Deck {
-    
+
+    String[] couleur = {"COEUR","CARREAU","PIQUE","TREFLE"};
+    String[] valeur = {"AS","ROI","DAME","VALET","DIX","NEUF","HUIT","SEPT","SIX","CINQ","QUATRE","TROIS","DEUX"};
+
+    public Card[] newRandomHand(){
+        Random random = new Random();
+
+        Card carte1 = new Card(valeur[random.nextInt(5)],couleur[random.nextInt(5)]);
+        Card carte2 = new Card(valeur[random.nextInt(5)],couleur[random.nextInt(5)]);
+        Card carte3 = new Card(valeur[random.nextInt(5)],couleur[random.nextInt(5)]);
+        Card carte4 = new Card(valeur[random.nextInt(5)],couleur[random.nextInt(5)]);
+        Card carte5 = new Card(valeur[random.nextInt(5)],couleur[random.nextInt(5)]);
+
+        Card[] hand = {carte1,carte2,carte3,carte4,carte5};
+        
+        return hand;
+    }
+
+    public 
+
+
+
 }
